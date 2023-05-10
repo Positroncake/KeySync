2aec1984b6a4c60e0d264f675b5f1ea7fc0a45b1
Positroncake <roncake@protonmail.com>
2023-05-10 15:00:24 +0000

    -

---
+++ Backend/Authentication.cs
@@ -0,0 +1,11 @@
+﻿using Npgsql;
+
+namespace Backend;
+
+public class Authentication
+{
+    public static void NewUser()
+    {
+        using var c = new NpgsqlConnection();
+    }
+}
---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeySync.Server.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public async Task<ActionResult> Register()
+    {
+        return Ok();
+    }
+}
